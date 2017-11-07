c98f1c407ebee935d195d4e2278afdcd13854a2a
j3rryj <jerryjong01@gmail.com>
2017-11-06 23:00:30 -0800

    webpack config for React

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve('build'),
+    filename: 'bundle.js',
+    libraryTarget: 'var',
+    library: 'EntryPoint',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$|\.jsx$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react'],
+      },
+    },
+    {
+      test: /.css$|.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+    {
+      test: /\.svg$/,
+      loader: 'svg-loader',
+    }],
+  },
+};
