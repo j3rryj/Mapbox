8452921cd0d2d86e8dbaf970eeea067d79c8ac90
j3rryj <jerryjong01@gmail.com>
2017-11-08 13:21:32 -0800

    serving css

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, './')));
 app.use('/build/*', express.static(path.join(__dirname, './build/bundle.js')));
+app.use('/src/styles/*', express.static(path.join(__dirname, './src/styles/styles.css')));
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
