ca63acd0d392783fa63e3f58fa101f649683ee90
j3rryj <jerryjong01@gmail.com>
2017-11-08 14:29:44 -0800

    added interactivity for clicking on locations

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,41 @@
-import React, { Component } from 'react';
-
-import '../styles/styles.css';
-
+import React from 'react';
 import { stores } from '../../sweetgreen.js';
 
-class Sidebar extends Component {
-  componentDidMount() {
-    console.log(stores);
-  }
-  render() {
+const Sidebar = (props) => {
+  let i = -1;
+  let listing = stores.features.map((feature) => {
+    i += 1;
     return (
-      <div className="sidebar">
-        <div className="heading">
-          <h1>Our locations</h1>
+      <div className="item" key={i} id={`listing-${i}`}>
+        <a
+          href='#'
+          className='title' 
+          onClick={() => {
+            props.flyToStore(feature);
+            props.createPopUp(feature);
+            var activeItem = document.getElementsByClassName('active');
+            if (activeItem[0]) {
+              activeItem[0].classList.remove('active');
+            }
+          }}>
+          {feature.properties.address}
+        </a>
+        <div className='details'>
+          {feature.properties.city} &middot; {feature.properties.phoneFormatted}
         </div>
-        <div id="listings" className="listings"></div>
       </div>
-    );
-  }
-}
+    )
+  })
+
+  return (
+    <div className="sidebar">
+      <div className="heading">
+        <h1>Our locations</h1>
+      </div>
+      <div id="listings" className="listings">{listing}</div>
+    </div>
+  );
+};
+
+
 export default Sidebar;
