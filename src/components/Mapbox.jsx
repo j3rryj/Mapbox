b8d2aa0b88fd1c5b8fc487c3a41df5ffa6cef00d
j3rryj <jerryjong01@gmail.com>
2017-11-08 13:22:20 -0800

    added source and one layer

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -1,59 +1,83 @@
 import React from 'react';
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVycnlqb25nIiwiYSI6ImNqOW93OXB0YzFnaHcyd240ZmlvMTc3eDYifQ.ZLuZbS7D2OcCUxT642-6xA';
+import { stores } from '../../sweetgreen.js';
+import Sidebar from './Sidebar.jsx';
 
 class Mapbox extends React.Component {
   componentDidMount() {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9',
-      center: [-118.2, 34],
-      zoom: 9
+      center: [-77.034084, 38.909671],
+      // initial zoom
+      zoom: 14
     });
+    this.map.on('load', () => {
+      this.map.addLayer({
+        id: 'locations',
+        type: 'symbol',
+        // Add a GeoJSON source containing place coordinates and information.
+        source: {
+          type: 'geojson',
+          data: stores
+        },
+        layout: {
+          'icon-image': 'restaurant-15',
+          'icon-allow-overlap': true,
+        }
+      });
+    });
+    this.buildLocationList(stores);
   }
 
   componentWillUnmount() {
     this.map.remove();
   }
 
+  buildLocationList(data) {
+  // Iterate through the list of data
+  for (let i = 0; i < data.features.length; i++) {
+    var currentFeature = data.features[i];
+    // Shorten data.feature.properties to just `prop` so we're not
+    // writing this long form over and over again.
+    var prop = currentFeature.properties;
+    // Select the listing container in the HTML and append a div
+    // with the class 'item' for each store
+    var listings = document.getElementById('listings');
+    var listing = listings.appendChild(document.createElement('div'));
+    listing.className = 'item';
+    listing.id = 'listing-' + i;
+
+    // Create a new link with the class 'title' for each store
+    // and fill it with the store address
+    var link = listing.appendChild(document.createElement('a'));
+    link.href = '#';
+    link.className = 'title';
+    link.dataPosition = i;
+    link.innerHTML = prop.address;
+
+    // Create a new div with the class 'details' for each store
+    // and fill it with the city and phone number
+    var details = listing.appendChild(document.createElement('div'));
+    details.innerHTML = prop.city;
+    if (prop.phone) {
+      details.innerHTML += ' &middot; ' + prop.phoneFormatted;
+    }
+  }
+}
+
   render() {
     const style = {
       height: '800px',
       width: '800px'
     };
-    return <div style={style} ref={el => this.mapContainer = el} />;
+    return (
+      <div style={{display: 'flex'}}>
+        <Sidebar />
+        <div className="map pad2" style={style} ref={el => this.mapContainer = el}>Map</div>
+      </div>
+    );
   }
 };
 
 export default Mapbox;
-
-// map.addControl(new MapboxGeocoder({
-//     accessToken: mapboxgl.accessToken
-// }));
-
-// map.on('load', function() {
-//     map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function(error, image) {
-//         if (error) throw error;
-//         map.addImage('cat', image);
-//         map.addLayer({
-//             "id": "points",
-//             "type": "symbol",
-//             "source": {
-//                 "type": "geojson",
-//                 "data": {
-//                     "type": "FeatureCollection",
-//                     "features": [{
-//                         "type": "Feature",
-//                         "geometry": {
-//                             "type": "Point",
-//                             "coordinates": [-118.2, 34]
-//                         }
-//                     }]
-//                 }
-//             },
-//             "layout": {
-//                 "icon-image": "cat",
-//                 "icon-size": 0.1
-//             }
-//         });
-//     });
-// });  
