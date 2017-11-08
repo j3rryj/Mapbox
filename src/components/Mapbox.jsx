2f71ead893799c190b406030ef5d4e7f9f0cb491
j3rryj <jerryjong01@gmail.com>
2017-11-08 14:29:27 -0800

    added interactivity for clicking on Map

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -4,6 +4,11 @@ import { stores } from '../../sweetgreen.js';
 import Sidebar from './Sidebar.jsx';
 
 class Mapbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.flyToStore = this.flyToStore.bind(this);
+    this.createPopUp = this.createPopUp.bind(this);
+  }
   componentDidMount() {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
@@ -27,44 +32,58 @@ class Mapbox extends React.Component {
         }
       });
     });
-    this.buildLocationList(stores);
+        // Add an event listener for when a user clicks on the map
+    this.map.on('click', function(e) {
+      // Query all the rendered points in the view
+      var features = this.map.queryRenderedFeatures(e.point, { layers: ['locations'] });
+      if (features.length) {
+        var clickedPoint = features[0];
+        // 1. Fly to the point
+        this.flyToStore(clickedPoint);
+        // 2. Close all other popups and display popup for clicked store
+        this.createPopUp(clickedPoint);
+        // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+        var activeItem = document.getElementsByClassName('active');
+        if (activeItem[0]) {
+          activeItem[0].classList.remove('active');
+        }
+        // Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
+        var selectedFeature = clickedPoint.properties.address;
+
+        for (var i = 0; i < stores.features.length; i++) {
+          if (stores.features[i].properties.address === selectedFeature) {
+            selectedFeatureIndex = i;
+          }
+        }
+        // Select the correct list item using the found index and add the active class
+        var listing = document.getElementById('listing-' + selectedFeatureIndex);
+        listing.classList.add('active');
+      }
+    });
   }
 
   componentWillUnmount() {
     this.map.remove();
   }
 
-  buildLocationList(data) {
-  // Iterate through the list of data
-  for (let i = 0; i < data.features.length; i++) {
-    var currentFeature = data.features[i];
-    // Shorten data.feature.properties to just `prop` so we're not
-    // writing this long form over and over again.
-    var prop = currentFeature.properties;
-    // Select the listing container in the HTML and append a div
-    // with the class 'item' for each store
-    var listings = document.getElementById('listings');
-    var listing = listings.appendChild(document.createElement('div'));
-    listing.className = 'item';
-    listing.id = 'listing-' + i;
-
-    // Create a new link with the class 'title' for each store
-    // and fill it with the store address
-    var link = listing.appendChild(document.createElement('a'));
-    link.href = '#';
-    link.className = 'title';
-    link.dataPosition = i;
-    link.innerHTML = prop.address;
-
-    // Create a new div with the class 'details' for each store
-    // and fill it with the city and phone number
-    var details = listing.appendChild(document.createElement('div'));
-    details.innerHTML = prop.city;
-    if (prop.phone) {
-      details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-    }
+  flyToStore(currentFeature) {
+    this.map.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 15
+    });
   }
-}
+
+  createPopUp(currentFeature) {
+    var popUps = document.getElementsByClassName('mapboxgl-popup');
+    // Check if there is already a popup on the map and if so, remove it
+    if (popUps[0]) popUps[0].remove();
+
+    var popup = new mapboxgl.Popup({ closeOnClick: false })
+      .setLngLat(currentFeature.geometry.coordinates)
+      .setHTML('<h3>Sweetgreen</h3>' +
+        '<h4>' + currentFeature.properties.address + '</h4>')
+      .addTo(this.map);
+  }
 
   render() {
     const style = {
@@ -73,7 +92,7 @@ class Mapbox extends React.Component {
     };
     return (
       <div style={{display: 'flex'}}>
-        <Sidebar />
+        <Sidebar flyToStore={this.flyToStore} createPopUp={this.createPopUp}/>
         <div className="map pad2" style={style} ref={el => this.mapContainer = el}>Map</div>
       </div>
     );
